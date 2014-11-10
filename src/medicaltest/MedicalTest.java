a4176c75ba590f35babb57feb08bfff26cc060e3
shahzeb <asif.shahzeb@hotmail.com>
2014-11-10 14:57:09 -0700

    Finished MedicalTest.Java. It takes in a Connection and prints something after processing regardless of success/fail.

--- src/medicaltest/MedicalTest.java
+++ src/medicaltest/MedicalTest.java
@@ -1,16 +1,21 @@
 package medicaltest;
 
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.sql.*;
 
 import common.UIO;
 
 public class MedicalTest {
 
-	public MedicalTest(){
+	public MedicalTest(Connection connection){
 		ioproc = new UIO();
+		this.connection = connection;
 	}
 	
 	UIO ioproc;
+	Connection connection;
 	
 	int health_care_number;
 	int employee_number;
@@ -28,7 +33,7 @@ public class MedicalTest {
 			verifyTest();
 			updateDB();
 		} catch (MedicalTestException mte) {
-			System.out.println(mte.toString());
+			System.out.println("Process failed: " + mte.toString());
 		}
 	}
 	
@@ -42,14 +47,43 @@ public class MedicalTest {
 		}
 	}
 	
-	private void verifyTest() {
+	private void verifyTest() throws MedicalTestException{
 		// Check if health_care_number was assigned a type_id by employee_number
 		// and that no test was previously done, i.e. results are null.
 		// If this is not the case, throw exception.
+		String query = String.format("SELECT *"
+								+ "FROM test_record tr" 
+								+ "WHERE tr.type_id = %d AND"
+								+ "tr.patient_no = %d AND"
+								+ "tr.employee_no = %d AND" 
+								+ "tr.result IS NULL",
+								type_id, health_care_number, employee_number);
+		
+		try {
+			if (connection.createStatement().executeQuery(query).getRow() == 0)
+				throw new MedicalTestException("Prescription not valid");
+		} catch (SQLException se) {
+			throw new MedicalTestException("Prescription not valid");
+		}
 	}
 	
-	private void updateDB() {
+	private void updateDB() throws MedicalTestException {
 		// Update rows with lab_name, results, and test_date.
+		String query = String.format("UPDATE test_record set"
+					+ "medical_lab = %s,"
+					+ "result = %s,"
+					+ "test_date = %s,"
+					+ "WHERE type_id = %d AND"
+					+ "patient_no = %d AND"
+					+ "employee_no = %d",
+					lab_name, results, getTestDateInSQLDateStringForm(), 
+					type_id, health_care_number, employee_number);
+
+		try {
+			connection.createStatement().executeQuery(query).getRow();
+		} catch (SQLException se) {
+			throw new MedicalTestException("Test not updated");
+		}
 	}
 	
 	private void getResultsInfo() throws MedicalTestException {
@@ -61,5 +95,9 @@ public class MedicalTest {
 			throw new MedicalTestException(iae.toString());
 		}
 	}
-
+	
+	/* Inspired by Picasso and Prescription.java */
+	private String getTestDateInSQLDateStringForm() {
+		return (new SimpleDateFormat("dd-MMM-YYYY")).format(test_date);
+	}
 }
