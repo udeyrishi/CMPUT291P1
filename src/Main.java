dcfd36728e282be40284ebef0ee2e864ea7a0934
Udey Rishi <udey.rishi@gmail.com>
2014-11-10 17:09:44 -0700

    Improved the main and the ConnectionManager classes.
    - Renamed the class "Connect" to "ConnectionManager".
    - Turned it into a regular class (vs. static).
    - Bug Fix: Server URL was incorrect.
    - Other misc. improvements.
    
    Testing and Verification Instructions:
      1. The ConnectionManager has been tested to be working
          on lab computers.
    Type: Bug Fix
    Type: Code Improvement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 import java.sql.*;
+
+import common.ConnectionManager;
 
 public class Main {
     
@@ -13,49 +14,63 @@ public class Main {
         // Print the welcome screen.
         
         System.out.println("Welcome to the Health Care Application System!\n");
-        System.out.println("Please enter '1' to initiate the Prescription program.");
-        System.out.println("Please enter '2' to initiate the Medical Test program.");
-        System.out.println("Please enter '3' to initiate the Patient Information Update program.");
-        System.out.println("Please enter '4' to initiate the Search Engine program.");
-        System.out.println("Please enter 'exit' to exit the program.");
+        System.out.println("Please enter '0' to initiate the Prescription program.");
+        System.out.println("Please enter '1' to initiate the Medical Test program.");
+        System.out.println("Please enter '2' to initiate the Patient Information Update program.");
+        System.out.println("Please enter '3' to initiate the Search Engine program.");
+        System.out.println("Please enter '4' to exit the program.");
     }
     
     public static String get_user_input() {
         // Get input from the user. Only integers between 1-4 will be accepted (for the 4 modes). 
-        // Entering 'exit' will quit the program.
+        // Entering 'q' will quit the program.
         
         String user_input = "";
         Scanner input_stream = new Scanner(System.in);
-        String[] acceptable_inputs = {"1", "2", "3", "4", "exit"};
-        String error_message = "Improper input. Please input an integer between 1 to 4, inclusive, or the word 'exit'.";
-
-        user_input = input_stream.next();
- 
-        while (!Arrays.asList(acceptable_inputs).contains(user_input)) {
-            System.out.println(error_message);
-            user_input = get_user_input();
-        }
+        ArrayList<String> acceptable_inputs = new ArrayList<String>(5);
+        acceptable_inputs.add("0"); 
+        acceptable_inputs.add("1"); 
+        acceptable_inputs.add("2"); 
+        acceptable_inputs.add("3"); 
+        acceptable_inputs.add("4");
         
+        String error_message = "Improper input. Please input an integer between 0 to 4, inclusive.";
+
+        while (true) {
+        	user_input = input_stream.next();
+        	if (acceptable_inputs.contains(user_input))
+        		break;
+        	else
+        		System.out.println(error_message);
+        }
+        input_stream.close();
         return user_input;
     }
-    
-    
-    
     
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         // Main program. Opens an application based on user selection. Opens the 'main' method of the selected application.
         
         String user_choice = "";
-        String[] dummy = {"arbitrary", "argument", "for", "main"};
+        //String[] dummy = {"arbitrary", "argument", "for", "main"};
         Boolean exit_flag = false;
         
         // Fetch the connection object from common.Connect.
-        Connection connection = new common.Connect().get_connection();
+        ConnectionManager con = new ConnectionManager();
+        Connection connection;
+        try {
+        	connection = con.get_connection();
+        }
+        catch (SQLException e) {
+        	// Failue to connect;
+        	System.out.println("Connection to server timed-out. Now exiting...");
+        	return;
+        }
         
         while (true) {
             initiate_welcome_screen();
             user_choice = get_user_input();
-            
+            exit_flag = true;
+            /*
             switch(user_choice) {
                 //case "1": new prescription.Presrciption().main(dummy); break;
                 //case "2": new medicaltest.MedicalTest().main(dummy); break;
@@ -64,11 +79,11 @@ public class Main {
                 //case "4": new searchengine.SearchEngine.main(dummy); break;
                 case "exit": exit_flag = true; break;
             }
-            
+            */
             user_choice = null;
             
             if (exit_flag) {
-                System.in.close();
+                //System.in.close();
                 connection.close();
                 System.out.println("\nThe program will now exit.\n");
                 break;
---
+++ src/common/ConnectionManager.java
@@ -0,0 +1,38 @@
+package common;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class ConnectionManager {
+	public ConnectionManager() {}
+	
+    private String get_username(Scanner in) {
+        // Returns a String containing a SQL-PLUS username, as inputed by the user.
+
+        System.out.println("Please enter your SQL Plus username.");
+        return in.nextLine();
+    }
+    
+    
+    private String get_password(Scanner in) {
+        // Returns a String containing a SQL-PLUS password, as inputed by the user.
+
+        System.out.println("Please enter your SQL Plus password.");
+        return in.nextLine();
+    }
+    
+    
+    public Connection get_connection() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+		Scanner in = new Scanner(System.in);
+    	String username = get_username(in);
+        String password = get_password(in); // Figure out a way to hide input
+        in.close();
+        String m_driverName = "oracle.jdbc.driver.OracleDriver";
+        Class<?> drvClass = Class.forName(m_driverName);
+        DriverManager.registerDriver((Driver)drvClass.newInstance());        
+        DriverManager.setLoginTimeout(5);
+        String url = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+        Connection con = DriverManager.getConnection(url, username, password);
+        return con;
+    }
+}
